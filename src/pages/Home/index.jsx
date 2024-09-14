9791f8d4a473f898fc3af14fabc35633164fbeec
Tatiana Zemliakova <tatzemliakova@gmail.com>
2024-09-14 19:11:09 +0200

    Ajout du bloc Menu de la page Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,57 +1,68 @@
 import photo from '../../assets/photo.svg'
 import WebDev from '../../assets/WebDev.svg'
-//import ellipse from '../../assets/ellipse'
-//import ordinateur from '../../assets/ordinateur.svg'
+import weblink from '../../assets/weblink.svg'
+import ordinateur from '../../assets/ordinateur.svg'
 import '../../style/home.scss'
 
 function Home() {
   return (
-    <div>
-      <div className="blocTitre">
-        <div className="blocTitre__titre">À propos</div>
-        <div className="blocTitre__bloc"></div>
-      </div>
-      <div className="blocBleu">
-        <div className="blocBleu__image">
-          <div className="blocBleu__image__ellipseBlanche">
-            <div className="blocBleu__image__ellipseRose"></div>
-          </div>
-          <img src={photo} alt="Tatiana" className="blocBleu__image__photo" />
+    <div className="home">
+      <div className="blocApropos">
+        <div className="blocTitre">
+          <div className="blocTitre__titre">À propos</div>
+          <div className="blocTitre__bloc"></div>
         </div>
-        <div className="blocBleu__contenu">
-          <div className="blocBleu__contenu__text">
-            <p>Je suis</p>
-            <img
-              src={WebDev}
-              alt="Web Developpeur"
-              className="blocBleu__contenu__text__dev"
-            />
+        <div className="blocBleu">
+          <div className="blocBleu__image">
+            <div className="blocBleu__image__ellipseBlanche">
+              <div className="blocBleu__image__ellipseRose"></div>
+            </div>
+            <img src={photo} alt="Tatiana" className="blocBleu__image__photo" />
           </div>
-          <p className="blocBleu__contenu__nom">
-            Tatiana
-            <br />
-            Youssoufa Saidou
-          </p>
+          <div className="blocBleu__contenu">
+            <div className="blocBleu__contenu__text">
+              <p>Je suis</p>
+              <img
+                src={WebDev}
+                alt="Web Developpeur"
+                className="blocBleu__contenu__text__dev"
+              />
+            </div>
+            <p className="blocBleu__contenu__nom">
+              Tatiana
+              <br />
+              Youssoufa Saidou
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-
-    /*(
       <div className="blocMenu">
-        <h1 className="blocMenu-titre">Portfolio</h1>
-        <div className="blocMenu-contenu">
+        <h1 className="blocMenu__titre">Portfolio</h1>
+        <div className="blocMenu__contenu1">
           <img
-            className="blocMenu-contenu-image"
+            className="blocMenu__contenu1__image"
             src={ordinateur}
             alt="La conception WEB"
           />
+          <div className="blocMenu__contenu1__blocs">
+            <div className="blocMenu__contenu1__blocs__gris">CV</div>
+            <div className="blocMenu__contenu1__blocs__bleu">Contacts</div>
+          </div>
         </div>
-        <div className="blocMenu-contenu-cv">
-          <div>CV</div>
-          <div>Contacts</div>
+
+        <div className="blocMenu__contenu2">
+          <div className="blocMenu__contenu2__blocRose">Mes compétences</div>
+          <div className="blocMenu__contenu2__blocNoir">
+            <img
+              className="blocMenu__contenu2__blocNoir__image"
+              src={weblink}
+              alt="Abstraction"
+            />
+          </div>
+          <div className="blocMenu__contenu2__blocBeige">Projets</div>
         </div>
       </div>
-    )*/
+    </div>
   )
 }
 
